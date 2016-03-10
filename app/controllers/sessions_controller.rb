e48f332cf7bfc427fb44b453cd9069aebebf2236
Chen Yang <yangc5@mail.uc.edu>
2016-03-09 19:23:50 -0600

    can login with facebook now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,27 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def auth
+    request.env['omniauth.auth']
+  end
+
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if auth['uid']
+      user = User.find_by(email: auth[:info][:email])
+      if user.nil?
+        user = User.create(name: auth[:info][:name], email: auth[:info][:email], password: "password")
+      end
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        log_in user
+        redirect_to user
+      else
+        flash.now[:danger] = 'Invalid email/password combination'
+        render 'new'
+      end
     end
   end
 
