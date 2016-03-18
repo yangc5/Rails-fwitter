5b7371c745761b5d0df225c2cd5a447a4b4e1ecf
Chen Yang <yangc5@mail.uc.edu>
2016-03-18 14:55:05 -0500

    the newly added li for micropost now identical to ones from backend

--- app/serializers/micropost_user_serializer.rb
+++ app/serializers/micropost_user_serializer.rb
@@ -1,3 +1,8 @@
 class MicropostUserSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :gravatar_url
+
+  def gravatar_url
+    "https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(object.email.downcase)}?s=50"
+  end
+
 end
